29cf2298f7be99e7139cb649835673b60e7d1fb1
yoav freund <yfreund@ucsd.edu>
2014-11-21 07:59:24 -0800

    finalizing for lecture 5

--- lecture_5_Mining-the-Social-Web/ipynb/DecibelPython27/settings.py
+++
@@ -1,9 +0,0 @@
-import pickle
-
-headers=pickle.load(open('/Users/yoavfreund/.Vault/Decibel.pkl','r'))
-
-applicationID = headers['DecibelAppID']
-applicationKey = headers['DecibelAppKey']
-apiAddress = 'http://decibel-rest-jazz.cloudapp.net/v1/'
-
-print applicationID,applicationKey
